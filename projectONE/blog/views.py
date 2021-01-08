70b99ecd971464f7d91bf66370dc5667b91b3a97
5thfader <53315778+chrisjeremy84@users.noreply.github.com>
2021-01-08 20:12:29 +0100

    Create, update and  delete posts

--- projectONE/blog/views.py
+++ projectONE/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, 
+                                DetailView, 
+                                CreateView,
+                                UpdateView,
+                                DeleteView)
 from django.http import HttpResponse
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        UserPassesTestMixin)
 from .models import Post
 
 # Create your views here.
@@ -25,5 +32,55 @@ def home(request):
     }
     return render(request, 'blog/Home.html', context)
 
+"""
+Here a listview is being created.
+By default the PostListView looks for a template of a certain type.
+And usually the template is named as  <app>/<model>_<Viewtype>.html
+In this case the template would be blog/Post_ListView.html
+However we can always change the template being looked for.
+In the current file 'views.py' you can assign the template to 
+the variable named template_name.
+"""
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #---<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post #Shows the details of the post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user#Checks for current User
+        return super().form_valid(form)#This runs the form after Cheking the user
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView, UserPassesTestMixin):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user#Checks for current User
+        return super().form_valid(form)#This runs the form after Cheking the user
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post #Shows the details of the post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 def about(request):
     return render(request, 'blog/About.html')
